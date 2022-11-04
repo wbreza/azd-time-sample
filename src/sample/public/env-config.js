71ea4daf5e31b2cfd9dde2fe3e0ccd3388ce5827
Wallace Breza <wallace.breza@microsoft.com>
2022-11-04 12:51:02 -0700

    Configure Azure Developer Pipeline

--- src/sample/public/env-config.js
+++ src/sample/public/env-config.js
@@ -1,4 +1,4 @@
 window.ENV_CONFIG = {
-REACT_APP_APPLICATIONINSIGHTS_CONNECTION_STRING: 'InstrumentationKey=d9895949-3ca7-4993-8d18-ca692076bf20;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/',
+REACT_APP_APPLICATIONINSIGHTS_CONNECTION_STRING: 'InstrumentationKey=75be30ad-f263-441d-8c91-d1ccf7a33441;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/',
 
 }
